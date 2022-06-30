2566a4aea8ea77068e71e45da39cbe70b15b1ca4
HossainRafi <hossainrafi71@gmail.com>
2022-06-30 22:17:54 +0600

    daisy ui added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "aqua",
+      "cupcake",
+    ],
   },
   plugins: [require("daisyui")],
 };
